cfd1a1d404a4cc9cae74ef83f244891b1c3adcde
Zeling Feng <zeling.feng@hotmail.com>
2017-11-28 21:51:31 +0800

    launch container

--- platform/src/tasks.py
+++ platform/src/tasks.py
@@ -3,30 +3,54 @@ from time import sleep
 from base64 import b64decode
 from tempfile import mkstemp, mkdtemp
 from docker import DockerClient
+from shutil import rmtree
 import os
+import errno
 import tarfile
+import smtplib
 
 
 app = Celery('tasks', broker='amqp://guest:guest@localhost', backend="amqp")
 
-
 @app.task
 def deploy(stu_id, submission):
-    content = b64decode(submission)
-    tmpdir = mkdtemp(prefix=stu_id)
-    (t, tarname) = mkstemp(prefix=stu_id, suffix='.tgz', dir=tmpdir)
-    os.write(t, content)
-    os.close(t)
-    with tarfile.open(name=tarname, mode='r:gz') as tar:
-        tar.extractall(path=tmpdir)
-    img_tag = '{}/chess/{}'.format('localhost:5000', stu_id)
+    try:
+        content = b64decode(submission)
+        tmpdir = mkdtemp(prefix=stu_id)
+        (t, tarname) = mkstemp(prefix=stu_id, suffix='.tgz', dir=tmpdir)
+        os.write(t, content)
+        os.close(t)
+        with tarfile.open(name=tarname, mode='r|gz') as tar:
+            tar.extractall(path=tmpdir)
+        img_tag = '{}/chess/{}'.format('localhost:5000', stu_id)
+        docker = DockerClient('unix:///var/run/docker.sock')
+        docker.images.build(path=os.path.join(tmpdir, 'agent'), tag=img_tag)
+        docker.images.push(img_tag)
+
+    except:
+        raise
+
+    finally:
+        try:
+            rmtree(tmpdir)
+        except OSError as e:
+            if e.errno != errno.ENOENT:
+                raise
+@app.task
+def launch(stu_id):
     docker = DockerClient('unix:///var/run/docker.sock')
-    docker.images.build(path=os.path.join(tmpdir, 'agent'), tag=img_tag)
-    docker.images.push(img_tag)
-    
+    if docker.containers.list(filters={'name': stu_id}):
+       raise RuntimeError('You have already launched your instance')
+    else:
+       img_tag = '{}/chess/{}'.format('localhost:5000', stu_id)
+       container = docker.containers.run(img_tag, auto_remove=True, cpu_count=1, detach=True, name=stu_id)
 
 @app.task
-def add(x, y):
-    sleep(3)
-    return x + y
-
+def sendmail(stu_id, subject, content):
+    smtp = smtplib.SMTP()
+    smtp.connect('smtp.live.com', 587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login('zeling.feng@hotmail.com', os.environ('MAIL_PASS'))
+    
