caf7ceea64fbcacf1ce20130ed09f82098e63cb5
zeling <zeling.feng@hotmail.com>
2017-12-12 15:08:27 +0800

    dumb agent

--- agent/src/agent.py
+++ agent/src/agent.py
@@ -1,8 +1,62 @@
 import chess
 
+def pop_count(i):
+    s = 0
+    while i:
+        s += 1
+        i &= i - 1
+    return s
+
+_p_scores = [0, 10, 30, 30, 50, 90, 900]
+_max_score = 1290
+
+def _evaluate(b, c):
+    # could be optimized into a matrix mult, but I choose to belive in pypy's JIT
+    score = 0
+    for ptype in range(chess.PAWN, chess.KING + 1):
+        mask = b.pieces_mask(ptype, c)
+        score += pop_count(mask) * _p_scores[ptype]
+    return score
+
+def evaluate(b):
+    return _evaluate(b, b.turn) - _evaluate(b, not b.turn)
+
+
+def _minimax(b, d=3, mmin=-_max_score, mmax=_max_score, is_max_node=True):
+    if not d: return evaluate(b)
+    if is_max_node:
+        score = mmin
+        for move in b.legal_moves:
+            b.push(move)
+            score_prime = _minimax(b, d-1, score, mmax, False)
+            b.pop()
+            if score_prime > score:
+                score = score_prime
+            if score > mmax: return mmax
+        return score
+    else:
+        score = mmax
+        for move in b.legal_moves:
+            b.push(move)
+            score_prime = _minimax(b, d-1, mmin, score, True)
+            b.pop()
+            if score_prime < score:
+                score = score_prime
+            if score < mmin: return mmin
+        return score
+
 def respond_to(fen):
-  # This is very stupid
-  board = chess.Board(fen)
-  import random
-  return str(random.choice(list(board.legal_moves)))
-  
+    b = chess.Board(fen)
+    s = -_max_score
+    bestmove = None
+    for move in b.legal_moves:
+        b.push(move)
+        min_score = _minimax(b, d=3, mmin=s, is_max_node=False)
+        b.pop()
+        if min_score > s: 
+            s = min_score
+            bestmove = move
+    return bestmove.uci()
+
+if __name__ == '__main__':
+    print(respond_to(chess.Board().fen()))
--- cli.py
+++ cli.py
@@ -6,8 +6,10 @@ from tempfile import mkstemp
 import tarfile
 import os
 from base64 import b64encode
+import json
 
 SERVER_URI = 'http://10.141.209.144:54321'
+HEADERS = { 'Content-Type': 'application/json' }
 
 def ask_cred():
     sys.stdout.write('Student Id: ')
@@ -29,12 +31,13 @@ def login():
 
 @cli.command()
 def list():
-    for r in requests.get('{SERVER_URI}/agents'.format(**globals())).json().repositories:
-        print r
+    for r in requests.get('{SERVER_URI}/agents'.format(**globals())).json()[u'repositories']:
+        click.echo(r)
 
 @cli.command()
+@click.argument('stu_id')
 @click.argument('path', default=os.path.join(os.getcwd(), 'agent'))
-def deploy(path):
+def deploy(stu_id, path):
     path = os.path.normpath(path)
     (fd, name) = mkstemp()
     if os.path.isfile(os.path.join(path, 'Dockerfile')):
@@ -47,17 +50,31 @@ def deploy(path):
             f.seek(0)
             content = b64encode(f.read())
         os.remove(name)
-        # int r = requests.post('{SERVER_URI}/deploy', data=json.dumps({ stu_id: stu_id, content: content }))
+        r = requests.post('{SERVER_URI}/deploy'.format(**globals()), data=json.dumps({'stu_id': stu_id, 'content': content}), headers=HEADERS)
+        if r.status_code == requests.codes.ok:
+            click.echo(r.text)
+        else:
+            click.echo(r.json()[u'reason'])
     else:
         click.echo('you should specify a directory with a Dockerfile')
 
 @cli.command()
-def start():
-    pass
+@click.argument('stu_id')
+def launch(stu_id):
+    r = requests.post('{SERVER_URI}/launch'.format(**globals()), data=json.dumps({'stu_id': stu_id}), headers=HEADERS)
+    if r.status_code == requests.codes.ok:
+        click.echo(r.text)
+    else:
+        click.echo(r.json()[u'reason'])
 
 @cli.command()
-def stop():
-    pass
+@click.argument('stu_id')
+def kill(stu_id):
+    r = requests.post('{SERVER_URI}/kill'.format(**globals()), data=json.dumps({'stu_id': stu_id}), headers=HEADERS)
+    if r.status_code == requests.codes.ok:
+        click.echo(r.text)
+    else:
+        click.echo(r.json()[u'reason'])
 
 if __name__ == '__main__':
     cli()
